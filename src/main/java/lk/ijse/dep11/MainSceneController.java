27ccfcb209fbd93660e52c56826e3019646132aa
shashikala <chandrasekaremsmh@gmail.com>
2023-08-22 23:30:15 +0530

    Exit and User Guide

--- src/main/java/lk/ijse/dep11/MainSceneController.java
+++ src/main/java/lk/ijse/dep11/MainSceneController.java
@@ -18,13 +18,30 @@ public class MainSceneController {
         Scene scene1 = new Scene(scene1Root);
         Stage stage = new Stage();
         stage.setScene(scene1);
-        //stage.setTitle("Text Editor");
+        stage.setTitle("Text Editor");
+        stage.setMaximized(true);
         stage.show();
     }
 
-    public void mnuitmExitOnAction(ActionEvent actionEvent) {
+    public void mnuitmExitOnAction(ActionEvent actionEvent) throws Exception{
+        AnchorPane scene1Root = FXMLLoader.load(getClass().getResource("/view/MainScene.fxml"));
+        Scene scene1 = new Scene(scene1Root);
+        Stage stage = new Stage();
+        stage.setScene(scene1);
+        Stage loginStage = (Stage) root.getScene().getWindow();
+        loginStage.close();
+
     }
 
-    public void mnuitmUserGuideOnAction(ActionEvent actionEvent) {
+    public void mnuitmUserGuideOnAction(ActionEvent actionEvent) throws Exception{
+        AnchorPane userGuideSceneRoot = FXMLLoader.load(getClass().getResource("/view/UserGuideScene.fxml"));
+        Scene userGuideScene = new Scene(userGuideSceneRoot);
+        Stage stage = new Stage();
+        stage.setScene(userGuideScene);
+        stage.setTitle("User Guide for Text Editor");
+        stage.show();
+        stage.setMaximized(true);
+
+
     }
 }
